554714a8a3ffafc9f184d23df2e8c25a7fb04223
theo <teddyhoover@gmail.com>
2021-03-30 14:09:33 -0500

    commit tree b1cfc71d2b6760ef57ffee3b1fbaf0064d6ecba9

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+function saturdayFun(activity='roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function(activity=`go to the office`) {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(str="*") {
+    return function(adj="special") {
+        return `You are ${str}${adj}${str}!`
+    }
+}
+
+const Calculator = {
+    add: (function(num1, num2) {
+        return num1 + num2
+    }),
+    subtract: (function(num1, num2) {
+        return num1 - num2
+    }),
+    multiply: (function(num1, num2) {
+        return num1 * num2
+    }),
+    divide: (function(num1, num2) {
+        return num1 / num2
+    })
+}
+
+function actionApplyer(i, arr) {
+    if (arr.length == 0) {
+        return i
+    } else {
+        arr.forEach(element => {
+            i = element(i)
+        });
+    }
+    return i
+}
